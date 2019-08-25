2530dd8993faa347a02d17fdb6fff3f915670b1e
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-08-25 09:47:01 -0400

    rearranged downloader structure

---
+++ src/dl.js
@@ -0,0 +1,66 @@
+const EventEmitter = require('events');
+const ffmpeg = require('fluent-ffmpeg');
+const shell = require('shelljs');
+const path = require('path');
+
+class Downloader extends EventEmitter {
+  constructor (tmpPath, i) {
+    super();
+
+    this.instance = i;
+
+    this._tmp = shell.tempdir();
+    this._stage = 1;
+
+    this._dlPath = path.join(this._tmp, tmpPath, this.instance);
+    
+    if (!shell.test('-e', path.join(this._tmp, tmpPath))) shell.mkdir(path.join(this._tmp, tmpPath));
+    if (shell.test('-e', this._dlPath)) shell.rm('-r', this._dlPath);
+    
+    shell.mkdir(this._dlPath);
+  }
+
+  _addListener () {
+    this._ffmpeg.once('progress', () => this.emit('status', this._stage === 1 ? 'downloading' : 'copying'));
+    this._ffmpeg.on('progress', data => {
+      if (data) {
+        this.emit('progress', (Math.min(data.percent, 100) / 100).toFixed(2));
+      }
+    });
+  }
+
+  async getMedia () {
+    throw 'No Media';
+  }
+
+  async start (url, subtitles, path) {
+    this.emit('status', 'starting');
+    const {link, subtitle} = await this.getMedia(url);
+    this._ffmpeg = ffmpeg(link).addOption('-c copy');
+
+    this._ffmpeg.addOutput(path.join(this._dlPath, 'download.mkv'));
+    
+    this._ffmpeg.on('end', () => {
+      this._ffmpeg = ffmpeg(path.join(this._dlPath, 'download.mkv'));
+      this._addListener();
+
+      if (subtitles && subtitle) {
+        this._ffmpeg.addInput(subtitle);
+        this._ffmpeg.addOption('-c copy');
+        this._ffmpeg.addOption('-c:s mov_text');
+      }
+      else this._ffmpeg.addOption('-c copy');
+      
+      this._ffmpeg.on('end', () => {
+        shell.rm('-r', this._dlPath);
+        this.emit('end');
+      }).save(path);
+      this._stage = 2;
+    });
+
+    this._addListener();
+    this._ffmpeg.run();
+  }
+}
+
+module.exports = Downloader;
