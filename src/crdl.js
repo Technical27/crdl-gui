2defa05645af42d1e42ed597e07423165508ccd0
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-08-26 16:58:01 -0400

    rewrote the downloader

--- src/crdl.js
+++ src/crdl.js
@@ -13,20 +13,24 @@ class CRDL extends Downloader {
     return JSON.parse(line.trim().replace('vilos.config.media = ', '').replace(';', ''));
   }
 
-  async getMedia (url) {
-    const html = await fetch(url);
-    const lines = await html.text();
-    let config, link, subtitle;
-    for (const line of lines.split('\n')) if (line.includes('vilos.config.media')) config = this._getMediaConfig(line);
-    if (!config) throw 'No link found';
-    for (const stream of config.streams) if (stream.format === 'adaptive_hls' && stream.hardsub_lang === null) link = stream.url;
-    try {
-      subtitle = config.subtitles[0].url.trim();
-    }
-    catch (e) {
-      subtitle = null;
-    }
-    return {link, subtitle};
+  getMedia (url) {
+    fetch(url)
+      .then(html => html.text())
+      .then(lines => {
+        return new Promise((resolve, reject) => {
+          let config, link, subtitle;
+          for (const line of lines.split('\n')) if (line.includes('vilos.config.media')) config = this._getMediaConfig(line);
+          if (!config) reject('No link found');
+          for (const stream of config.streams) if (stream.format === 'adaptive_hls' && stream.hardsub_lang === null) link = stream.url;
+          try {
+            subtitle = config.subtitles[0].url.trim();
+          }
+          catch (e) {
+            subtitle = null;
+          }
+          resolve({link, subtitle});
+        });
+      });
   }
 }
 
--- src/dl.js
+++ src/dl.js
@@ -3,6 +3,9 @@ const ffmpeg = require('fluent-ffmpeg');
 const shell = require('shelljs');
 const path = require('path');
 
+ffmpeg.setFfmpegPath(require('ffmpeg-static').path); // eslint-disable-line global-require
+ffmpeg.setFfprobePath(require('ffprobe-static').path); // eslint-disable-line global-require
+
 class Downloader extends EventEmitter {
   constructor (tmpPath, i) {
     super();
@@ -24,42 +27,50 @@ class Downloader extends EventEmitter {
     this._ffmpeg.once('progress', () => this.emit('status', this._stage === 1 ? 'downloading' : 'copying'));
     this._ffmpeg.on('progress', data => {
       if (data) {
-        this.emit('progress', (Math.min(data.percent, 100) / 100).toFixed(2));
+        this.emit('progress', Math.min(data.percent, 100).toFixed(2));
       }
     });
   }
 
-  async getMedia () {
-    throw 'No Media';
+  getMedia () {
+    return Promise.reject('No Media');
   }
 
-  async start (url, subtitles, path) {
+  start (url, subtitles, output) {
     this.emit('status', 'starting');
-    const {link, subtitle} = await this.getMedia(url);
-    this._ffmpeg = ffmpeg(link).addOption('-c copy');
+    this.getMedia(url)
+      .then(media => {
+        this._ffmpeg = ffmpeg(media.url).addOption('-c copy');
 
-    this._ffmpeg.addOutput(path.join(this._dlPath, 'download.mkv'));
+        this._ffmpeg.addOutput(path.join(this._dlPath, 'download.mkv'));
     
-    this._ffmpeg.on('end', () => {
-      this._ffmpeg = ffmpeg(path.join(this._dlPath, 'download.mkv'));
-      this._addListener();
+        this._ffmpeg.on('end', () => {
+          this._ffmpeg = ffmpeg(path.join(this._dlPath, 'download.mkv'));
+          this._addListener();
 
-      if (subtitles && subtitle) {
-        this._ffmpeg.addInput(subtitle);
-        this._ffmpeg.addOption('-c copy');
-        this._ffmpeg.addOption('-c:s mov_text');
-      }
-      else this._ffmpeg.addOption('-c copy');
+          if (subtitles && media.subtitle) {
+            this._ffmpeg.addInput(media.subtitle);
+            this._ffmpeg.addOption('-c copy');
+            this._ffmpeg.addOption('-c:s mov_text');
+          }
+          else this._ffmpeg.addOption('-c copy');
       
-      this._ffmpeg.on('end', () => {
-        shell.rm('-r', this._dlPath);
-        this.emit('end');
-      }).save(path);
-      this._stage = 2;
-    });
+          this._ffmpeg.on('end', () => {
+            shell.rm('-r', this._dlPath);
+            this.emit('end');
+          }).save(output);
+          this._stage = 2;
+        });
 
-    this._addListener();
-    this._ffmpeg.run();
+        this._addListener();
+    
+        try {
+          this._ffmpeg.run();
+        }
+        catch (e) {
+          console.log(e);
+        }
+      });
   }
 }
 
