f7194dbbc9020fded29bb6af53b597c86e032a31
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-09-28 14:18:18 -0400

    added que for downloading links

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 const {ipcRenderer} = require('electron');
 const $ = require('jquery');
+let instances = 0;
+const que = [];
 
 ipcRenderer.on('update', (event, type, instance, status) => {
   if (!$(`#${type}${instance}`).length) $('#log').append($(`<span id=${type}${instance}></span>`));
   $(`#${type}${instance}`).html(`${type} instance ${instance}: ${status}`);
 });
 
-ipcRenderer.on('end', (event, type, instance) => $(`#${type}${instance}`).remove());
+ipcRenderer.on('end', (event, type, instance) => {
+  if (que.length) {
+    ipcRenderer.send('start', 'crdl', que.shift());
+  }
+  else instances--;
+  $(`#${type}${instance}`).remove();
+});
 
 $(document).ready(() => {
   $('#addLink').click(() => {
     const $link = $('#newLink');
     console.log('starting');
-    ipcRenderer.send('start', 'crdl', $link.val());
+    if (instances <= 2) {
+      instances++;
+      ipcRenderer.send('start', 'crdl', $link.val());
+    }
+    else que.push($link.val());
     $link.val('');
   });
 });
